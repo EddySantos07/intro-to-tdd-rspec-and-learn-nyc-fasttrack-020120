1d42925424d38e0a583d4ccf980f168a1b913d93
Eddy Santos <eagle100065@gmail.com>
2020-02-06 06:41:29 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,4 @@
 # def current_age_for_birth_year (age)
 #   return Date.current.year - age
 # end
-puts "What year were you born?"
-birth_year = gets.to_i
-
-users_age = current_age_for_birth_year(birth_year)
-
-puts "You are: " + users_age.to_s + " years old."
+current_age_for_birth_year method
